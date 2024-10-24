a970040d94176f0a716817219a42eec269ca3a43
Kolibrius <iu.yakovlev@my-kolibri.com>
2024-10-24 12:41:45 +0300

    sdfdfs

--- aiBeTrade-stock/aibetrade_stock/stockPublisher/calculationResult.py
+++ aiBeTrade-stock/aibetrade_stock/stockPublisher/calculationResult.py
@@ -20,33 +20,32 @@ accounts = [
         "api_secret": os.getenv('API_BYBIT_SEC_CR'),
         "strategy_id": "roman_strat",
         "strategy_name": "ABT BITS PRO_PAR",
-        "start_deposit": 4920,  # Начальный депозит для первого аккаунта 
-        "channel_id": os.getenv('ID_CH_CR')  # ID Telegram канала для первого аккаунта
+        "start_deposit": 5000  # Начальный депозит для первого аккаунта
     },
     {
         "api_key": os.getenv('API_BYBIT_CR_1'),
         "api_secret": os.getenv('API_BYBIT_SEC_CR_1'),
         "strategy_id": "constantin_strat",
         "strategy_name": "ABT BITS PRO_NOM",
-        "start_deposit": 4050,  # Начальный депозит для второго аккаунта
-        "channel_id": os.getenv('ID_CH_CR')  # ID Telegram канала для второго аккаунта
+        "start_deposit": 4050  # Начальный депозит для второго аккаунта
     },
     {
         "api_key": os.getenv('API_BYBIT_CR_2'),
         "api_secret": os.getenv('API_BYBIT_SEC_CR_2'),
         "strategy_id": "news_strat",
         "strategy_name": "ABT BITS PRO_NEWS",
-        "start_deposit": 2350,  # Начальный депозит для третьего аккаунта
-        "channel_id": os.getenv('ID_CH_CR')  # ID Telegram канала для третьего аккаунта
+        "start_deposit": 2350  # Начальный депозит для второго аккаунта
     }
+    
 ]
 
 # Настройки для Telegram
 URL_BOT = 'https://api.telegram.org/bot'
 TELEGRAM_TOKEN = os.getenv('API_BOT_CR')  # Токен вашего Telegram бота
+CHANNEL_ID = os.getenv('ID_CH_CR')  # ID вашего канала
 
 # Функция для публикации в Telegram через запрос к API
-def publish_to_telegram(profit, totalProfit, days, is_successful, strategy_name, channel_id):
+def publish_to_telegram(profit, totalProfit, days, is_successful, strategy_name):
     try:
         # Определяем картинку и текст
         if is_successful:
@@ -68,26 +67,15 @@ def publish_to_telegram(profit, totalProfit, days, is_successful, strategy_name,
                 f"Number of Trading Days: <b>{days}</b>"
             )
 
-        # Добавляем кнопки с ссылками
-        keyboard = {
-            "inline_keyboard": [
-                [
-                    {"text": "🚀 ABT Bits Pro Bot", "url": "https://t.me/aibetradecombot"},
-                    {"text": "🛠⁉️ ABT Support", "url": "https://t.me/abtsupportbot"}
-                ]
-            ]
-        }
-
         url = f'{URL_BOT}{TELEGRAM_TOKEN}/sendPhoto'
 
         # Открываем изображение и отправляем запрос на API Telegram
         with open(image_path, 'rb') as image_file:
             files = {'photo': image_file}
             data = {
-                'chat_id': channel_id,
+                'chat_id': CHANNEL_ID,
                 'caption': message_text,
-                'parse_mode': 'HTML',
-                'reply_markup': json.dumps(keyboard)
+                'parse_mode': 'HTML'
             }
             response = requests.post(url, files=files, data=data)
 
@@ -98,7 +86,104 @@ def publish_to_telegram(profit, totalProfit, days, is_successful, strategy_name,
     except Exception as e:
         logger.error(f"Error publishing to Telegram: {e}")
 
-# Остальные функции остались неизменными: save_balance_to_file, get_previous_balance, count_days_in_file, calculate_profit
+# Функция для записи нового баланса и даты в файл
+def save_balance_to_file(balance, strategy_id, filename=None):
+    if filename is None:
+        filename = f"balance_data_{strategy_id}.json"
+    
+    filepath = filename
+    current_date = datetime.now().strftime('%d.%m.%Y')
+
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            if isinstance(data, dict):
+                data = [data]
+    except FileNotFoundError:
+        data = []  # Если файла нет, начинаем с пустого списка
+
+    # Флаг для отслеживания, найдена ли запись за текущий день
+    record_found = False
+
+    # Проверяем, есть ли запись за текущий день
+    for entry in data:
+        if entry['date'] == current_date:
+            entry['balance'] = balance  # Обновляем баланс для текущего дня
+            logger.info(f"Balance for {current_date} has been updated.")
+            record_found = True
+            break
+
+    # Если запись не найдена, добавляем новую
+    if not record_found:
+        new_entry = {'balance': balance, 'date': current_date}
+        data.append(new_entry)
+        logger.info(f"Balance for {current_date} has been added.")
+
+    try:
+        with open(filepath, 'w') as file:
+            json.dump(data, file, indent=4)
+        logger.info(f"Balance saved to {filepath}")
+    except Exception as e:
+        logger.error(f"Error saving balance to file: {e}")
+
+# Функция для чтения баланса за предыдущий день
+def get_previous_balance(strategy_id, filename=None):
+    if filename is None:
+        filename = f"balance_data_{strategy_id}.json"
+    
+    filepath = filename
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            if isinstance(data, dict):
+                data = [data]
+            prev_date = (datetime.now() - timedelta(days=1)).strftime('%d.%m.%Y')
+            for entry in data:
+                if entry['date'] == prev_date:
+                    return float(entry['balance'])
+            logger.info(f"No balance entry found for {prev_date}.")
+            return None
+    except FileNotFoundError:
+        logger.error(f"File not found: {filepath}")
+        return None
+    except Exception as e:
+        logger.error(f"Error reading balance from file: {e}")
+        return None
+
+# Функция для подсчета количества записей в файле балансов
+def count_days_in_file(strategy_id, filename=None):
+    if filename is None:
+        filename = f"balance_data_{strategy_id}.json"
+    
+    filepath = filename
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            if isinstance(data, dict):
+                data = [data]
+            return len(data)  # Возвращаем количество записей (количество дней)
+    except FileNotFoundError:
+        logger.error(f"File not found: {filepath}")
+        return 0
+    except Exception as e:
+        logger.error(f"Error counting days in file: {e}")
+        return 0
+
+# Функция расчета профита
+def calculate_profit(resultBalance, preBalance, start_deposit):
+    if preBalance is None:
+        logger.warning("No previous balance data available for calculation.")
+        return None, None
+    
+    profit = (resultBalance / preBalance - 1) * 100
+    totalProfit = (resultBalance / start_deposit - 1) * 100
+
+    logger.info(f"Текущий баланс: {resultBalance}")
+    logger.info(f"Баланс предыдущий день: {preBalance}")
+    logger.info(f"Результат (%): {profit}")
+    logger.info(f"Общий результат (%): {totalProfit}")
+
+    return round(profit, 2), round(totalProfit, 2)
 
 # Основная функция для каждого аккаунта
 def main_for_account(account):
@@ -142,7 +227,7 @@ def main_for_account(account):
                 if profit is not None and totalProfit is not None:
                     # Определяем успех или провал и публикуем сообщение в Telegram
                     is_successful = resultBalance > preBalance
-                    publish_to_telegram(profit, totalProfit, days, is_successful, account["strategy_name"], account["channel_id"])
+                    publish_to_telegram(profit, totalProfit, days, is_successful, account["strategy_name"])
                 
             except (KeyError, IndexError) as e:
                 logger.error(f'Error extracting resultBalance: {e}')
@@ -150,7 +235,6 @@ def main_for_account(account):
             logger.error(f'Error in response for {account["strategy_name"]}: {response["retMsg"]}')
     except Exception as e:
         logger.error(f"Error in Bybit API session for {account['strategy_name']}: {e}")
-
 # Планирование выполнения задачи каждые 60 минут
 def main():
     for account in accounts:
