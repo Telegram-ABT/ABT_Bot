bfceeb13a7e2b032d1b8edaf0715e1a146b5cab5
Kolibrius <iu.yakovlev@my-kolibri.com>
2024-10-26 19:22:22 +0300

    empire spam

--- aiBeTrade-stock/aibetrade_stock/removeSpam/handlerRemoveSpam.py
+++ aiBeTrade-stock/aibetrade_stock/removeSpam/handlerRemoveSpam.py
@@ -116,7 +116,7 @@ def check_ref(msg:Message):
         'HAMSTER' in text or 'BIO' in text or 'bio' in text or 'AlRDR0P' in text or 
         'airfrop' in text or '❗️❗️❗️❗️❗️❗️❗️❗️' in text or '👍👍👍👍' in text or 
         'Click To Claim' in text or 'AIRDOP' in text or '🆕🆕🆕🆕' in text or 
-        '$DОGS' in text or 'JetTon' in text or 'NOTCOIN' in text
+        '$DОGS' in text or 'JetTon' in text or 'NOTCOIN' in text or 'Empire' in text or 'EMPIRE' in text
     ):        # Проверяем отсутствие aibetrade
             return True
     return False
