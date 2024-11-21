2b77cde0837c7f351c7afc1ac6ed714d9be240d0
Kolibrius <iu.yakovlev@my-kolibri.com>
2024-11-21 11:53:27 +0300

    Ready to use

--- aiBeTrade-stock/aibetrade_stock/scaner/analits.py
+++ aiBeTrade-stock/aibetrade_stock/scaner/analits.py
@@ -12,22 +12,20 @@ def process_pnl_selection(bot,chat_id,threshold, case_name=None):
     # Удаляем все записи из kogan_sell_position
     bot.send_message(chat_id, "Начало формирования базы данных")
     sell_position_collection.delete_many({})
-    bot.send_message(chat_id, "Удалены все записи из kogan_sell_position")
 
     # Выбираем акции с PNL больше заданного порога
     query = {"case_name": case_name} if case_name else {}
     shares = case_share_collection.find(query)
     for share in shares:
-        bot.send_message(chat_id, f"Акция {share['case_name']} {share['symbolId']}")
         quantity = float(share.get('quantity', 0))
         average_price = float(share.get('averagePrice', 0))
         price = float(share.get('price', 0))
-        bot.send_message(chat_id, f"Количество {quantity} средняя цена {average_price} цена {price}")
 
         if quantity > 0 and price > 0:
             pnl = (1 - (quantity * average_price) / (quantity * price)) * 100
             if pnl > threshold:
-                # Добавляем акции в kogan_sell_position
+                bot.send_message(chat_id, f"Количество {quantity} средняя цена {average_price} цена {price} P&L {pnl}")
+              # Добавляем акции в kogan_sell_position
                 sell_position_collection.insert_one(share)
 
     return "База акций сформирована"
