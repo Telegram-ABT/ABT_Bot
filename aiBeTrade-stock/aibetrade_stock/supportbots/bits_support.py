73b39c33fd44c4ea36c99ea204214569e35408a1
Kolibrius <iu.yakovlev@my-kolibri.com>
2024-12-05 23:43:45 +0300

    ыфвааыв

--- aiBeTrade-stock/aibetrade_stock/supportbots/bits_support.py
+++ aiBeTrade-stock/aibetrade_stock/supportbots/bits_support.py
@@ -421,7 +421,27 @@ def handle_callback_query(call):
                 reply_markup=create_main_menu(user_lang)
             )
             return
-
+        
+        elif call.data.startswith("exchange_"):
+            logger.info(f"Обработка create_account для пользователя {call.from_user.id}")
+            # Инициализируем состояние пользователя
+            if not hasattr(bot, 'user_states'):
+                bot.user_states = {}
+            
+            bot.user_states[call.from_user.id] = {
+                'step': 'key',
+                'user_id': call.from_user.id
+            }
+            
+            # Вызываем handle_new_connection из bits_status
+            handle_new_connection(
+                message=call.message,
+                bot=bot,
+                state=bot.user_states[call.from_user.id],
+                lang=user_lang,
+                user_id=call.from_user.id
+            )
+            return
         # Обработка информационного меню
         if call.data == "info":
             # Удаляем предыдущее сообщение
