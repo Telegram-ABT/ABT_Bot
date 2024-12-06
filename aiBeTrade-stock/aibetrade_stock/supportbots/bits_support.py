97462f6035fd48e5f52c38ef08ead00c9465ab53
Kolibrius <iu.yakovlev@my-kolibri.com>
2024-12-06 16:49:28 +0300

    ret

--- aiBeTrade-stock/aibetrade_stock/supportbots/bits_support.py
+++ aiBeTrade-stock/aibetrade_stock/supportbots/bits_support.py
@@ -241,8 +241,11 @@ def handle_status(message):
     }
     
     bot.send_message(message.chat.id, status_texts.get(user_lang, status_texts['en']))
+    
     status_texts = get_statistics(bot,message.chat.id,user_lang)
-    bot.send_message(message.chat.id, status_texts)
+    
+    bot.send_message(message.chat.id, status_texts,parse_mode='HTML', reply_markup=create_main_menu(user_lang))
+
 @bot.message_handler(commands=['info'])
 def handle_info(message):
     user_settings = bits_user_settings.find_one({'user_id': message.from_user.id})
